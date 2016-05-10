eca84211cc555a8612df0628668a794a798b88b6
60 <1096364408@qq.com>
2016-05-11 00:36:20 +0800

    quick sort

--- javascript/sort/quick_sort.js
+++ javascript/sort/quick_sort.js
@@ -8,25 +8,30 @@ var quick_sort = function (a, lo, hi) {
         var k = partition(a, lo, hi);
         quick_sort(a, lo, k - 1);
         quick_sort(a, k + 1, hi);
+
     }
     return a;
 };
 
 var partition = function (a, lo, hi) {
-    var k = lo;
-    lo++;
-    while (lo < hi) {
-        while (a[lo] < a[k] && lo < hi) {
-            lo++;
+    var i = lo + 1,
+        j = hi;
+    while (i < j) {
+        while (a[lo] >= a[i] && i < j) {
+            i++;
         }
-        while (a[hi] > a[k] && lo < hi) {
-            hi--;
+        while (a[lo] < a[j] && i < j) {
+            j--;
         }
-        swap(a, lo, hi);
+        swap(a, i, j);
     }
-    swap(a, lo, k);
-    console.log(a);
-    return lo;
+    if (a[i] > a[lo]) {
+        swap(a, lo, i - 1);
+        return i - 1;
+    } else {
+        swap(a, lo, i);
+        return i;
+    }
 };
 
 var swap = function (a, i, j) {
@@ -37,4 +42,4 @@ var swap = function (a, i, j) {
 
 
 console.log(quick_sort([9, 8, 7, 6, 5, 4, 3, 2, 1], 0, 8));
-//console.log(quick_sort([9, 4, 2, 1, 8, 23, 4, 5, 6, 7, 33, 222, 111, 4, 6, 8, 90], 0, 16));
+console.log(quick_sort([9, 4, 2, 1, 8, 23, 4, 5, 6, 7, 33, 222, 111, 4, 6, 8, 90], 0, 16));
